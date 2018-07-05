c38d81664d287b2f2cec53328a72b6689ae20395
Diogo Perillo <diogo.perillo@gmail.com>
2018-07-05 20:29:04 -0300

    Add minumum batch

--- scripts/distribution.js
+++ scripts/distribution.js
@@ -67,7 +67,7 @@ const fetchNextBatch = async () => {
     } else {
       return {
         entries: [],
-        hasMore: true,
+        hasMore: res.body.pendingSubscribers !== 0,
       }
     }
   } else {
