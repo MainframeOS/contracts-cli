aed3c5fa1d994ef72b4ff7fc2f9ed2aaa8689105
Adam Clarke <adam@mainframe.com>
2018-09-05 14:44:52 +0100

    update ledger integration to work with latest firmware

--- src/wallet.js
+++ src/wallet.js
@@ -7,13 +7,12 @@ import HDWalletProvider from 'truffle-hdwallet-provider'
 import HDWalletProviderPK from 'truffle-hdwallet-provider-privkey'
 import ProviderEngine from 'web3-provider-engine'
 import RpcSubprovider from 'web3-provider-engine/subproviders/rpc'
-import LedgerWalletSubproviderFactory from 'ledger-wallet-provider'
+import Transport from '@ledgerhq/hw-transport-node-hid'
+import createLedgerSubprovider from '@ledgerhq/web3-subprovider'
 import { prompt } from 'inquirer'
 
 import type { EthNetwork } from './types'
 import { log } from './cli-utils'
-
-const LEDGER_ROOT_PATH = `44'/60'/0'/0`
 
 const decodeKeystore = async (path?: string) => {
   const questions = [
@@ -74,7 +73,7 @@ const requestAccountSelection = async (accounts: Array<string>) => {
 export const openWallet = async (
   ethNetwork: EthNetwork,
   rpcUrl: string,
-): Promise<Web3> => {
+): Promise<{ web3: Web3, account: string }> => {
   const answers = await prompt([
     {
       type: 'list',
@@ -83,100 +82,69 @@ export const openWallet = async (
       choices: ['Private Key', 'Mnemonic', 'Ledger', 'Keystore File'],
     },
   ])
-  let provider
-  let web3
-  switch (answers.wallet) {
-    case 'Private Key': {
-      const key = await requestPrivateKey()
-      provider = new HDWalletProviderPK(key, rpcUrl)
-      web3 = new Web3(provider)
-      break
+  if (answers.wallet === 'Ledger') {
+    const engine = new ProviderEngine()
+    const web3 = new Web3(engine)
+    try {
+      const ledgerIndexAnswers = await prompt([
+        {
+          type: 'input',
+          name: 'accountIndex',
+          default: 0,
+          message: 'Ledger accounts selection offset: ',
+        },
+      ])
+      const networkId = ethNetwork === 'testnet' ? 3 : 1
+      const getTransport = () => Transport.create()
+      const ledgerWalletSelectedProvider = createLedgerSubprovider(
+        //$FlowFixMe Transport type
+        getTransport,
+        {
+          networkId,
+          accountsLength: 5,
+          accountsOffset: Number(ledgerIndexAnswers.accountIndex),
+        },
+      )
+      engine.addProvider(ledgerWalletSelectedProvider)
+      engine.addProvider(new RpcSubprovider({ rpcUrl }))
+      engine.start()
+      const accounts = await web3.eth.getAccounts()
+      const accountIndex = await requestAccountSelection(accounts)
+      const account = accounts[accountIndex]
+      return { web3, account }
+    } catch (err) {
+      log.warn(err)
+      log.info(`Error connecting to Ledger, please make sure:\n
+      - You have entered your pin and unlocked your ledger\n
+      - Selected Ethereum wallet\n
+      - Have browser support turned off`)
+      throw err
     }
-    case 'Keystore File': {
-      let decodedKey = await decodeKeystore()
-      if (decodedKey.slice(0, 2) === '0x') {
-        decodedKey = decodedKey.slice(2, decodedKey.length)
-      }
-      provider = new HDWalletProviderPK(decodedKey, rpcUrl)
-      web3 = new Web3(provider)
-      break
-    }
-    case 'Mnemonic': {
-      const mnemonic = await requestMnemonic()
-      provider = new HDWalletProvider(mnemonic, rpcUrl)
-      web3 = new Web3(provider)
-      break
-    }
-    case 'Ledger':
-    default: {
-      const engine = new ProviderEngine()
-      web3 = new Web3(engine)
-      try {
-        const ledgerWalletSubProvider = await LedgerWalletSubproviderFactory()
-
-        // If needing to select high index account on ledger ask for input
-
-        const choices = ['select from first 10', 'enter account index']
-        const answers = await prompt([
-          {
-            type: 'list',
-            name: 'accountSelect',
-            message: 'Ledger account select: ',
-            choices,
-          },
-        ])
-
-        let selectedPath
-
-        if (choices.indexOf(answers.accountSelect) === 0) {
-          // Select ledger account from list
-          const listAccounts: {
-            [string]: string,
-          } = await ledgerWalletSubProvider.ledger.getMultipleAccounts(
-            LEDGER_ROOT_PATH,
-            0,
-            10,
-          )
-          const accountAddresses = Object.values(listAccounts)
-          const accountPaths = Object.keys(listAccounts)
-          // $FlowFixMe: Object.values return Array<any>
-          const accountIndex = await requestAccountSelection(accountAddresses)
-          selectedPath = accountPaths[accountIndex]
-        } else {
-          // Select ledger account from input index
-          const ledgerIndexAnswers = await prompt([
-            {
-              type: 'input',
-              name: 'accountIndex',
-              message: 'Ledger account select: ',
-            },
-          ])
-          const accounts = await ledgerWalletSubProvider.ledger.getMultipleAccounts(
-            LEDGER_ROOT_PATH,
-            Number(ledgerIndexAnswers.accountIndex) - 1,
-            1,
-          )
-          selectedPath = Object.keys(accounts)[0]
+  } else {
+    let provider
+    switch (answers.wallet) {
+      case 'Keystore File': {
+        let decodedKey = await decodeKeystore()
+        if (decodedKey.slice(0, 2) === '0x') {
+          decodedKey = decodedKey.slice(2, decodedKey.length)
         }
-
-        // Set selected account as provider
-        const networkId = ethNetwork === 'testnet' ? 3 : 1
-        const ledgerWalletSelectedProvider = await LedgerWalletSubproviderFactory(
-          () => networkId,
-          selectedPath,
-        )
-        engine.addProvider(ledgerWalletSelectedProvider)
-        engine.addProvider(new RpcSubprovider({ rpcUrl }))
-        engine.start()
-      } catch (err) {
-        log.warn(err)
-        log.info(`Error connecting to Ledger, please make sure:\n
-        - You have entered your pin and unlocked your ledger\n
-        - Selected Ethereum wallet\n
-        - Have browser support turned off`)
-        process.exit()
+        provider = new HDWalletProviderPK(decodedKey, rpcUrl)
+        break
+      }
+      case 'Mnemonic': {
+        const mnemonic = await requestMnemonic()
+        provider = new HDWalletProvider(mnemonic, rpcUrl)
+        break
+      }
+      case 'Private Key':
+      default: {
+        const key = await requestPrivateKey()
+        provider = new HDWalletProviderPK(key, rpcUrl)
+        break
       }
     }
+    const web3 = new Web3(provider)
+    const account = await web3.eth.getCoinbase()
+    return { web3, account }
   }
-  return web3
 }
